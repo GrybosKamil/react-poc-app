d580866fe5a8ae3346995b051907cf0efccc37a0
Kamil Grybos <grybos.kamil@gmail.com>
2021-03-29 08:39:10 +0200

    Give content fill height

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import Header from "./Header"
+import Footer from "./Footer"
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div className="content">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
+        </div>
       </header>
+      <Footer />
     </div>
   );
 }
