3a7a47ba2de479112de1c782bcc01161f8468b94
Kamil Grybos <grybos.kamil@gmail.com>
2021-04-16 20:50:15 +0200

    Add theme to Footer and Header

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+
+import ThemeContext from "./context/theme/ThemeContext";
+
 const Footer = () => {
+  const { selectedTheme } = useContext(ThemeContext);
+
   return (
     // <div className="footer col-12">
-    <div className="footer">Footer</div>
+    <div className="footer" style={{ color: selectedTheme.theme.textColor }}>
+      Footer
+    </div>
   );
 };
 
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+
+import ThemeContext from "./context/theme/ThemeContext";
+
 const Header = () => {
+  const { selectedTheme } = useContext(ThemeContext);
+
+  console.log(selectedTheme);
+
   return (
     // <div className="header col-12">
-    <div className="header">Header</div>
+    <div className="header" style={{ color: selectedTheme.theme.textColor }}>
+      Header
+    </div>
   );
 };
 
