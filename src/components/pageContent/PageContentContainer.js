ff656470442a9eed6992b6414b5c7799af7ffb82
Kamil Grybos <grybos.kamil@gmail.com>
2021-04-23 23:13:11 +0200

    Clean

--- src/components/pageContent/PageContentContainer.js
+++ src/components/pageContent/PageContentContainer.js
@@ -1,6 +1,6 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 
-import ThemeContext from "../../context/theme/ThemeContext";
+// import ThemeContext from "../../context/theme/ThemeContext";
 
 import Sidebar from "../sidebar/Sidebar";
 import PageContent from "./PageContent";
@@ -8,7 +8,7 @@ import PageContent from "./PageContent";
 import "./PageContentContainer.scss";
 
 const PageContentContainer = () => {
-  const { selectedTheme } = useContext(ThemeContext);
+  // const { selectedTheme } = useContext(ThemeContext);
 
   return (
     <div className="page-content-container container">
